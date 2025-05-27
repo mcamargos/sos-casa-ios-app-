284648d26792c4089e0dbaff1ab51dbdd7674f1a
Matheus Camargos <camargosdantas@gmail.com>
2025-05-27 11:55:37 -0300

    Initial Commit

---
+++ SOS_Casa/SOS_CasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOS_CasaApp.swift
+//  SOS_Casa
+//
+//  Created by Matheus Camargos on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SOS_CasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SOS_CasaTests/SOS_CasaTests.swift
@@ -0,0 +1,17 @@
+//
+//  SOS_CasaTests.swift
+//  SOS_CasaTests
+//
+//  Created by Matheus Camargos on 27/05/25.
+//
+
+import Testing
+@testable import SOS_Casa
+
+struct SOS_CasaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
